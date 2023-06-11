a838c1a04714197042210718f7ed83ea263bd922
yuki3738 <y.minamiya@gmail.com>
2023-06-11 23:08:47 +0900

    20. await/asyncの使い方について学ぼう

--- 03_js_basic/090_promise/main.js
+++ 03_js_basic/090_promise/main.js
@@ -1,17 +1,21 @@
 let a = 0;
 // console.log(a);
 
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    a = 1;
-    resolve(a)
-    // reject(a)
-  }, 2000);
-}).then((b) => {
-  console.log(b);
-  return b;
-}).then((b) => {
-  console.log(b);
-}).catch((c) => {
-  console.log(`catchが実行`, c)
-})
+init();
+async function init() {
+  try {
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        a = 1;
+        // resolve(a)
+        reject(a)
+      }, 2000);
+    })
+      console.log(result);
+  } catch(e) {
+    console.log(`catchが実行`, e)
+  }
+  // }).catch((c) => {
+    // console.log(`catchが実行`, c)
+  // })
+}
